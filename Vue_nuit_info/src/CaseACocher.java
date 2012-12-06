2ef1640be0afeb0680f5526ebb2a367ddb831fa7
Wyvern <Wyvern@172.19.10.87>
2012-12-06 21:49:09 +0100

    Ajout classes

--- Vue_nuit_info/src/CaseACocher.java
+++ Vue_nuit_info/src/CaseACocher.java
@@ -1,4 +1,41 @@
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class CaseACocher {
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+
+/**
+ * Classe de la case à cocher, 
+ * Sur le principe la case à cocher va se voir affecter un paramètre, ce parametre représente la 
+ * catégorie à afficher si l'user appuie dessus. 
+ * 
+ * @author Wyvern
+ *
+ */
+@SuppressWarnings("serial")
+public class CaseACocher extends JPanel{
+	
+	public CaseACocher(final String[] typeAAfficher)
+	{
+		 final JTextArea textArea = new JTextArea();
+		    textArea.setText("Click Me!");
+		    textArea.addMouseListener(new MouseAdapter() {
+		        public void mouseClicked(MouseEvent e) {
+		          if (e.getButton() == MouseEvent.NOBUTTON) {
+		            textArea.setText("No button clicked...");
+		          } /*else if (e.getButton() == MouseEvent.BUTTON1) {
+		            textArea.setText("Button 1 clicked...");
+		          } else if (e.getButton() == MouseEvent.BUTTON2) {
+		            textArea.setText("Button 2 clicked...");
+		          } else if (e.getButton() == MouseEvent.BUTTON3) {
+		            textArea.setText("Button 3 clicked...");
+		          }*/
+
+		          System.out.println("Number of click: " + e.getClickCount());
+		          System.out.println("Click position (X, Y):  " + e.getX() + ", " + e.getY() + typeAAfficher);
+		        }
+		      });
+	}
 
 }
---
+++ Vue_nuit_info/test_fenetre.java
@@ -0,0 +1,17 @@
+/**
+ * Classe de test de la fentre de renseignement
+ * @author Wyvern
+ *
+ */
+
+
+class test_fenetre {
+	
+	 public static void main(String[] arg)
+	 {
+		fenetre_renseigner nouvfenetre = new fenetre_renseigner();
+		
+		
+	 }
+
+}

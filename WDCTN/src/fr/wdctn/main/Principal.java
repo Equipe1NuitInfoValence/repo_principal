b1949190cbfb7e0a2798ad09de81cc80896152ef
LordLuffyX <loic.chaufournais@gmail.com>
2012-12-07 04:22:18 +0100

    - Suppression classe vue

---
+++ WDCTN/src/fr/wdctn/main/Principal.java
@@ -0,0 +1,22 @@
+package fr.wdctn.main;
+
+import fr.wdctn.classes.Information;
+import fr.wdctn.classes.InterVue;
+
+public class Principal implements InterVue
+{
+	Information info;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+	}
+
+	public void setInfo(Information info)
+	{
+		this.info = info;
+		
+	}
+
+}
